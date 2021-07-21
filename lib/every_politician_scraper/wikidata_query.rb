a1fab342bfd906dd84bf89ef1b53adde210a4fea
Tony Bowden <tony@tmtm.com>
2021-07-21 12:03:58 +0300

    0.0.7 Class for the Wikidata list of members of a Cabinet
    Rather than each scaper replicating this, standardise it into a class
    that can be used everywhere, and then updated in one place when
    necessary.

--- lib/every_politician_scraper/wikidata_query.rb
+++ lib/every_politician_scraper/wikidata_query.rb
@@ -36,4 +36,71 @@ module EveryPoliticianScraper
       @response ||= Scraped::Request.new(url: query_url, headers: headers).response
     end
   end
+
+  # Wikidata's list of the members of a Cabinet
+  class WikidataCabinet
+    require 'pathname'
+    require 'json'
+
+    def initialize(config_filename)
+      @config_filename = config_filename
+    end
+
+    def csv
+      @csv ||= WikidataQuery.new(query, agent).csv
+    end
+
+    private
+
+    attr_reader :config_filename
+
+    def config_filepath
+      Pathname.new(config_filename)
+    end
+
+    def env
+      @env ||= JSON.parse(config_filepath.read, symbolize_names: true)
+    end
+
+    def agent
+      env[:agent] || raise('No agent provided')
+    end
+
+    def cabinet
+      env[:cabinet] || raise('No cabinet ID provided')
+    end
+
+    def lang
+      env[:lang] || 'en'
+    end
+
+    def query
+      <<~SPARQL
+        SELECT (STRAFTER(STR(?item), STR(wd:)) AS ?wdid) ?name (STRAFTER(STR(?positionItem), STR(wd:)) AS ?pid) ?position
+        WHERE {
+          BIND (wd:#{cabinet} AS ?cabinet) .
+          ?cabinet wdt:P31 ?parent .
+
+          # Positions currently in the cabinet
+          ?positionItem p:P361 ?ps .
+          ?ps ps:P361 ?parent .
+          FILTER NOT EXISTS { ?ps pq:P582 [] }
+
+          # Who currently holds those positions
+          ?item wdt:P31 wd:Q5 ; p:P39 ?held .
+          ?held ps:P39 ?positionItem ; pq:P580 ?start .
+          FILTER NOT EXISTS { ?held pq:P582 [] }
+
+          OPTIONAL { ?held prov:wasDerivedFrom/pr:P1810 ?sourceName }
+          OPTIONAL { ?item rdfs:label ?enLabel FILTER(LANG(?enLabel) = "#{lang}") }
+          BIND(COALESCE(?sourceName, ?enLabel) AS ?name)
+
+          OPTIONAL { ?positionItem wdt:P1705 ?nativeLabel }
+          OPTIONAL { ?positionItem rdfs:label ?positionLabel FILTER(LANG(?positionLabel) = "#{lang}") }
+          BIND(COALESCE(?nativeLabel, ?positionLabel) AS ?position)
+        }
+        ORDER BY ?positionLabel ?began
+      SPARQL
+    end
+  end
 end
