68280c02d61bf773409bd0a6a4b4107f99588797
Tony Bowden <tony@tmtm.com>
2021-08-07 13:10:46 +0300

    0.1.0 add a CabinetMemberList scraper class
    Most cabinet scrapers follow the same format, so create a default they
    can inherit from to remove the amount of cut'n'paste required, and make
    sure they all do the same things the same way by default.

--- lib/every_politician_scraper/scraper_data.rb
+++ lib/every_politician_scraper/scraper_data.rb
@@ -13,7 +13,7 @@ end
 module EveryPoliticianScraper
   # standardise the interface to running a scraper
   class ScraperData
-    def initialize(urls, klass: nil, headers: {})
+    def initialize(urls, klass: MemberList::Members, headers: {})
       @urls = [urls].flatten
       @klass = klass
       @headers = headers
@@ -25,16 +25,7 @@ module EveryPoliticianScraper
 
     private
 
-    attr_reader :urls, :headers
-
-    # Allow either fallback, for backwards compatibility
-    def klass
-      return @klass if @klass
-
-      ['MemberList::Members', 'Legislature::Members'].map do |klass|
-        Object.const_get(klass) if Object.const_defined?(klass)
-      end.compact.first
-    end
+    attr_reader :urls, :headers, :klass
 
     def data
       @data ||= urls.flat_map do |url|
@@ -59,6 +50,43 @@ module EveryPoliticianScraper
   class FileData < ScraperData
     def request(url)
       Scraped::Request.new(url: url, headers: headers, strategies: [LocalFileRequest])
+    end
+  end
+end
+
+class CabinetMemberList
+  # base class for an individual member
+  class Member < Scraped::HTML
+    field :name do
+    end
+
+    field :position do
+    end
+  end
+
+  # base class for the list of members
+  class Members < Scraped::HTML
+    field :members do
+      member_container.flat_map do |member|
+        data = fragment(member => member_class).to_h
+        [data.delete(:position)].flatten.map { |posn| data.merge(position: posn) }
+      end
+    end
+
+    def member_class
+      ::CabinetMemberList::Member
+    end
+  end
+end
+
+class MemberList
+  # details for an individual member
+  class Member < CabinetMemberList::Member; end
+
+  # The page listing all the members
+  class Members < CabinetMemberList::Members
+    def member_class
+      Member
     end
   end
 end
--- lib/every_politician_scraper/version.rb
+++ lib/every_politician_scraper/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module EveryPoliticianScraper
-  VERSION = '0.0.24'
+  VERSION = '0.1.0'
 end
