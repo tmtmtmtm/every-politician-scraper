5e8a3fd728f114c9a5a72015a080511890e3264b
Tony Bowden <tony@tmtm.com>
2023-05-05 08:36:59 +0300

    0.16.0 Add InfoboxEN
    Pull the Peppercat infobox parsing code into a new library, and make
    sure it passes the old infobox parsing code from here. The goal is to
    harmonise these more fully over time, but for now the new version will
    only handle enwiki infoboxes, whereas the old one here also handles
    Slovenian.

---
+++ lib/every_politician_scraper/infobox_en.rb
@@ -0,0 +1,146 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'every_politician_scraper/wpdates'
+require 'pry'
+
+class String
+  def deordinaled
+    ordinal = '\d+[snrt][tdh]'
+    tidy.gsub(/^#{ordinal} (and|&) #{ordinal} /, '').gsub(/^#{ordinal} /, '')
+  end
+end
+
+module InfoboxEN
+  class Mandate
+    POSITION = %w[office order title succession jr/sr parliament
+                  state_house state_senate state_assembly assembly
+                  constituency_mp constituency_am amabassador_from].freeze
+    BEGAN = %w[term_start termstart].freeze
+    ENDED = %w[term_end termend].freeze
+    TERM = %w[term reign].freeze
+
+    def initialize(origjson)
+      @origjson = origjson
+    end
+
+    def to_h
+      {
+        order:         origjson.keys.first[/(\d+)$/, 1].to_i,
+        positionLabel: position_label.deordinaled,
+        startDate:     start_date,
+        endDate:       end_date,
+      }
+    end
+
+    private
+
+    attr_reader :origjson
+
+    # strip off the position number
+    def json
+      @json ||= origjson.transform_keys { |key| key.sub(/\d+$/, '') }
+    end
+
+    def first_of_type(arr)
+      json.values_at(*arr).compact.first
+    end
+
+    def position
+      first_of_type(POSITION) || {}
+    end
+
+    # There's a lot of extra logic in the Officeholder template
+    #   https://en.wikipedia.org/w/index.php?title=Template:Infobox_officeholder/office&action=edit
+    def position_label
+      return "ambassador to #{json.dig('country', 'text') || '?'}" if json.key?('ambassador_from')
+
+      return "#{json.dig('parliament', 'text')} MP for #{json.dig('constituency_mp', 'text')}" if
+        json.key?('constituency_mp') && json.key?('parliament')
+      return "MP for #{json.dig('constituency_mp', 'text')}" if json.key?('constituency_mp')
+      return "Member of the #{json.dig('assembly', 'text')} Assembly" if json.key?('assembly')
+      return 'Senator' if json.key?('jr/sr')
+      return "#{json.dig('parliament', 'text')} MP" if json.key?('parliament')
+      return "#{json.dig('state_house', 'text')} State Representative" if json.key?('state_house')
+      return "#{json.dig('state_legislature', 'text')} State Legislator" if json.key?('state_legislature')
+      return "#{json.dig('state_senate', 'text')} State Senator" if json.key?('state_senate')
+      return "#{json.dig('state_assembly', 'text')} State Assembly Member" if json.key?('state_assembly')
+      return "US Representative from #{json.dig('state', 'text')}" if json.key?('state') && json.key?('constituency')
+
+      position['text'].to_s.tidy
+    end
+
+    def term
+      first_of_type(TERM)
+    end
+
+    def term_parts
+      return [] unless term
+      return term['text'].split(' to ', 2) if term['text'].include? ' to '
+
+      term['text'].split(/\s*[-–—]\s*/, 2)
+    end
+
+    def began
+      first_of_type(BEGAN) || {}
+    end
+
+    def ended
+      first_of_type(ENDED) || {}
+    end
+
+    def raw_start
+      began['text'] || term_parts[0]
+    end
+
+    def raw_end
+      ended['text'] || term_parts[1]
+    end
+
+    def start_date
+      return if raw_start.to_s.empty?
+
+      WikipediaDate.new(raw_start.to_s.tidy).to_s rescue raw_start.tidy
+    end
+
+    def end_date
+      return if raw_end.to_s.empty?
+
+      WikipediaDate.new(raw_end.to_s.tidy).to_s rescue raw_end.tidy
+    end
+  end
+
+  class JSON
+    def initialize(raw)
+      @raw = raw
+    end
+
+    # If a mandate has no positionLabel, fill it from the previous one
+    def mandates
+      raw_mandates.each_with_index do |mandate, idx|
+        mandate[:positionLabel] = raw_mandates[idx - 1][:positionLabel] if
+          mandate[:positionLabel].to_s.empty? && !idx.zero?
+      end
+    end
+
+    private
+
+    attr_reader :raw
+
+    def parsed
+      @parsed ||= ::JSON.parse(raw)
+    end
+
+    def infobox_lines
+      parsed['sections'].flat_map { |section| section['infoboxes'] }.compact.reduce(&:merge)
+    end
+
+    def grouped
+      infobox_lines.group_by { |key, _| key.match(/\d*$/)[0].to_i }.sort_by(&:first).map(&:last).map(&:to_h)
+    end
+
+    def raw_mandates
+      grouped.map { |entry| InfoboxEN::Mandate.new(entry).to_h rescue {} } rescue []
+    end
+  end
+end
---
+++ test/infobox_en_test.rb
@@ -0,0 +1,136 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'test_helper'
+require 'pathname'
+require 'pry'
+
+describe InfoboxEN::JSON do
+  let(:pathname)  { Pathname.new("test/data/#{datafile}") }
+  let(:infobox)   { InfoboxEN::JSON.new(pathname.read) }
+  let(:mandates) { infobox.mandates.reject { |data| data[:positionLabel].to_s.empty? }.sort_by { |data| data[:startDate].to_s } }
+
+  describe 'Annabel Goldie' do
+    let(:datafile) { 'AG.json' }
+
+    it 'has 6 mandates' do
+      assert_equal 6, mandates.count
+    end
+
+    it 'has a sensible latest position' do
+      mandate = mandates.last
+      assert_equal 'Minister of State for Defence', mandate[:positionLabel]
+      assert_equal '2019-07-26', mandate[:startDate]
+      assert_nil mandate[:endDate]
+    end
+
+    it 'handles holding the same position twice' do
+      leader = mandates.select { |posn| posn[:positionLabel] == 'Leader of the Scottish Conservative Party in the Scottish Parliament' }
+      assert_equal 2, leader.count
+    end
+  end
+
+  describe 'Osea Naiqamu' do
+    let(:datafile) { 'ON.json' }
+
+    it 'handles termstart as well as term_start' do
+      assert_equal 1, mandates.count
+      assert_equal '2018-11-21', mandates.first[:startDate]
+    end
+  end
+
+  describe 'Moon Sung-wook' do
+    let(:datafile) { 'MS.json' }
+
+    it 'handles going straight to office1 with no office' do
+      assert_equal 4, mandates.count
+    end
+  end
+
+  describe 'Jeong_Kyeong-doo' do
+    let(:datafile) { 'JK.json' }
+
+    it 'skips a different first infobox' do
+      assert_equal 3, mandates.count
+    end
+  end
+
+  describe 'Silvio Schembri' do
+    let(:datafile) { 'SS.json' }
+
+    it 'copes with being called Order rather than Office' do
+      assert_equal 2, mandates.count
+    end
+  end
+
+  describe 'Edward Zammit Lewis' do
+    let(:datafile) { 'EZL.json' }
+
+    it 'handles month+year only dates' do
+      assert_equal '2020-01', mandates[4][:startDate]
+    end
+
+    it 'splits "X to Y" term fields' do
+      assert_equal '2019-07', mandates[3][:startDate]
+      assert_equal '2020-01', mandates[3][:endDate]
+    end
+
+    it 'splits "X - Y" term fields' do
+      assert_equal '2014-04', mandates[2][:startDate]
+      assert_equal '2017-06', mandates[2][:endDate]
+    end
+  end
+
+  describe 'Eddie Fenech Adami' do
+    let(:datafile) { 'EFA.json' }
+
+    it 'handles both "office" and "order"' do
+      assert_equal 'Prime Minister of Malta', mandates[0][:positionLabel]
+    end
+  end
+
+  describe 'Teten Masduki' do
+    let(:datafile) { 'TM.json' }
+
+    it 'handles "office" and "order" on the same page' do
+      assert_equal 2, mandates.count
+      assert_equal 'Presidential Chief of Staff', mandates[0][:positionLabel]
+      assert_equal 'Minister for Cooperatives and SMEs', mandates[1][:positionLabel]
+    end
+  end
+
+  describe 'George Young' do
+    let(:datafile) { 'GY.json' }
+
+    it 'handles more than 10 offices' do
+      assert_equal 16, mandates.count
+      assert_includes mandates[1][:positionLabel], 'Ealing Acton'
+      assert_includes mandates[15][:positionLabel], 'Lord-in-waiting'
+    end
+  end
+
+  describe 'Zasia binti Sirin' do
+    let(:datafile) { 'ZbS.json' }
+
+    it 'handles other date separators' do
+      assert_equal 1, mandates.count
+      assert_includes mandates[0][:startDate], '2011'
+      assert_includes mandates[0][:endDate], '2016'
+    end
+  end
+
+  describe 'ʻAisake Eke' do
+    let(:datafile) { 'AE.json' }
+
+    it 'handles normal data for constituency_MP roles' do
+      assert_equal 2, mandates.count
+      assert_equal '2010-11-25', mandates[0][:startDate]
+      assert_equal '2017-11-16', mandates[0][:endDate]
+    end
+
+    it 'rewrites constituency MP offices' do
+      assert_equal 'MP for Tongatapu 5', mandates[0][:positionLabel]
+    end
+  end
+end
